b93f6a04ab0580666eab4f3697ce22d9570078c1
muchinta <muchinta@LIN66008028.corp.capgemini.com>
2022-05-17 21:07:00 +0530

    update swagger dependency

--- src/main/java/com/example/demo/FoodRecipeApplication.java
+++ src/main/java/com/example/demo/FoodRecipeApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 @SpringBootApplication
 public class FoodRecipeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodRecipeApplication.class, args);
+	}
+	
+	@Bean
+	public Docket myswaggerbean(){
+	return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.demo.controller")).build();
 	}
 
 }
