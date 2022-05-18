607888988727f9990f41389c251541ea2203322d
muchinta <muchinta@LIN66008028.corp.capgemini.com>
2022-05-18 20:42:48 +0530

    Added Security feature

---
+++ src/main/java/com/example/demo/security/FoodRecipeSecurity.java
@@ -0,0 +1,35 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class FoodRecipeSecurity extends WebSecurityConfigurerAdapter {
+
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+        .antMatchers("/swagger-ui/**", "/FoodRecipeDemo/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+	}
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("user"))
+                .authorities("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+}
